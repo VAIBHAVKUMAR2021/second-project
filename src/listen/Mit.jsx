00c9831f4e0fdd1c063b251e829b4ccd68f10fe8
vaibhavkumar <kumarvaibhav8735@gmail.com>
2025-10-28 21:48:44 +0530

    errors

---
+++ src/listen/Mit.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Mit = () => { let [task,setTask] = useState("")  //to store the value of single task , after that the empty string of useState is for the initial state
+
+let [list,setList] = useState([])  //to store the multiple value of task...
+
+let handleChange = (e) =>{
+    setTask(e.target.value)
+}
+
+let handleSubmit =(e) =>{
+    e.preventDefault()
+    if(!task){                     // if user didn't passs the task or send it empty
+        alert("Please Enter Some Task")
+    }else{
+        setList([...list,task])   // spread operator => so that it stores the value together
+        setTask("")
+    }
+}
+
+  return (
+    <>
+<h2>ToDoList</h2>
+<form onSubmit={handleSubmit}>
+    <input type='text' placeholder='Enter your task' value={task} onChange={handleChange}/>
+    <button>+</button>
+</form>
+{
+    list.map((value,index)=>{                  //for displaying the task on server
+        return(<Fragment key={index}>
+      <h3>{value}</h3>
+           <span> <MdDelete /></span>
+     </Fragment>)
+
+    })
+}
+    </>
+
+    
+    
+  )
+}
+
+export default Mit
